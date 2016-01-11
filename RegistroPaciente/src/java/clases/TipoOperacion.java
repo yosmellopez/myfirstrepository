273f6883f2b01ca7fef0f3cbe2b6e96261381806
Postgrado <Postgrado@10.22.10.231>
2016-01-11 18:40:56 -0500

    Todo OK

---
+++ RegistroPaciente/src/java/clases/TipoOperacion.java
@@ -0,0 +1,60 @@
+package clases;
+
+import clasesUtiles.SerializadorTipoOperacion;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tipo_operacion")
+public class TipoOperacion implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_tipo_operacion")
+    @Basic(optional = false)
+    private Integer idTipoOperacion;
+
+    @Column(name = "tipo", length = 255)
+    private String tipo;
+
+    @OneToMany(mappedBy = "tipoOperacion", fetch = FetchType.EAGER)
+    @JsonSerialize(using = SerializadorTipoOperacion.class)
+    private List<TipoOperacionRecurso> tipoOperacionRecursos;
+
+    public Integer getIdTipoOperacion() {
+        return idTipoOperacion;
+    }
+
+    public void setIdTipoOperacion(Integer idTipoOperacion) {
+        this.idTipoOperacion = idTipoOperacion;
+    }
+
+    public List<TipoOperacionRecurso> getTipoOperacionRecursos() {
+        return tipoOperacionRecursos;
+    }
+
+    public void setTipoOperacionRecursos(List<TipoOperacionRecurso> tipoOperacionRecursos) {
+        this.tipoOperacionRecursos = tipoOperacionRecursos;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+}
